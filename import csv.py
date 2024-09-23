810804481604cd64674ed67dce787b95a74c86f8
kacire <95851507+kaciret@users.noreply.github.com>
2024-09-23 10:04:42 -0600

    Idk xd

---
+++ import csv.py
@@ -0,0 +1,10 @@
+import csv
+
+def import_csv(filename):
+    with open(filename, 'rk') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    return data
+
+student_data = import_csv('Students.csv')
+print(student_data) 
